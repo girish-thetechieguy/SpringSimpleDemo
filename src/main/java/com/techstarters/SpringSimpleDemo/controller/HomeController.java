e062759beb97c1a2e64fbe36367b20145879ef90
girish-thetechieguy <girish.thetechieguy@gmail.com>
2023-03-24 23:00:13 +0800

    added the lomboks to write some logs and check merge operations with other branch

--- src/main/java/com/techstarters/SpringSimpleDemo/controller/HomeController.java
+++ src/main/java/com/techstarters/SpringSimpleDemo/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.techstarters.SpringSimpleDemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
 
     @RequestMapping("/")
     public String goToHome(){
+        log.info("This is the method called on everytime Home page is called");
         return "This is Demo Sample! ";
     }
 
