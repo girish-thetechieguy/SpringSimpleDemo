36bc255536f12180ee4c633908a9e1454604767d
girish-thetechieguy <girish.thetechieguy@gmail.com>
2023-03-24 22:41:42 +0800

    Created the first build with inital setup

---
+++ src/test/java/com/techstarters/SpringSimpleDemo/SpringSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstarters.SpringSimpleDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleDemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
